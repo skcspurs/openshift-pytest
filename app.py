747743a80ea3b2da32a71b3a73f13bbecf25c36a
skcspurs <acjackson3@gmail.com>
2019-03-03 20:07:46 -0500

    removed start delay

--- app.py
+++ app.py
@@ -381,7 +381,6 @@ def play(station_id):
 
 if __name__ == '__main__':
     # 38.9885%2C-76.791
-    time.sleep(180)
     EPGGRABBER.daemon = True
     EPGGRABBER.start()
     app.run(host='0.0.0.0', port=8080, debug=True)
